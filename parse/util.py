c9d952b55dc741eb1d4f06b09ba6ebc335db7aee
liudanqing <liudanqing1996@hotmail.com>
2022-08-16 11:50:02 +0200

    use window server file path on windows pc

--- parse/util.py
+++ parse/util.py
@@ -1,15 +1,16 @@
+import requests
 from nltk.parse.corenlp import CoreNLPServer
-
+import json
 
 def corenlp_server_start(path_to_jar=None,
                          path_to_models_jar=None):
     # Only in testing phase
-    path_to_jar = '/home/danki/Studium/Thesis/stanford_parser/jars/stanford-corenlp-4.5.0.jar'
-    path_to_models_jar = '/home/danki/Studium/Thesis/stanford_parser/jars/stanford-corenlp-4.5.0-models.jar'
+    path_to_jar = 'C:/Users/liuda/Documents/Files/StudiumInDtl/Masterarbeit/corenlp/stanford-corenlp-4.5.0/stanford-corenlp-4.5.0.jar'
+    path_to_models_jar = 'C:/Users/liuda/Documents/Files/StudiumInDtl/Masterarbeit/corenlp/stanford-corenlp-4.5.0/stanford-corenlp-4.5.0-models.jar'
     if not path_to_jar:
-        path_to_jar = input("input the path to stanford-corenlp-4.5.0.jar\n")
+        path_to_jar = input("input the path to stanford-corenlp-4.5.0.jar/n")
     if not path_to_models_jar:
-        path_to_models_jar = input("input the path to stanford-corenlp-4.5.0-model.jar\n")
+        path_to_models_jar = input("input the path to stanford-corenlp-4.5.0-model.jar/n")
     # Server
     server = CoreNLPServer(path_to_jar=path_to_jar,
                            path_to_models_jar=path_to_models_jar)
