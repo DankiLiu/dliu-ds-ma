9e961fe38376e10d25e79af0be7d98e75bacfc98
liudanqing <liudanqing1996@hotmail.com>
2022-10-24 10:07:42 +0200

    text and labels are lists instead of strings

--- pretrain/jointslu_dataset.py
+++ pretrain/jointslu_dataset.py
@@ -23,8 +23,20 @@ DiaBatchDictKey = Literal[
     'batch_text',  # b * L
     'batch_labels'  # B * L
 ]
-f = open("../data/jointslu_dict.json")
-labels = json.load(f)
+if False:
+    # Create labels dictionary
+    f = open("../data/jointslu/labels.csv")
+    import pandas as pd
+    labels_list = pd.read_csv(f, usecols=["simplified label"]).values
+    labels = list(set(item for sublist in labels_list for item in sublist))
+    labels_dict = {labels[i]: i for i in range(len(labels))}
+    # save into a file
+    with open("../data/jointslu/pre-train/labels.json", 'w') as f:
+        json.dump(labels_dict, f, indent=4)
+# Load labels
+f = open("../data/jointslu/pre-train/labels.json")
+labels_dict = json.load(f)
+f.close()
 DatasetSplitName = Literal["train", "val", "test"]
 DiaSample = Dict[str, Union[torch.Tensor, str, int]]
 DiaBatch = Dict[str, Union[List, Tensor]]
@@ -53,34 +65,31 @@ class JointSluDataset(Dataset):
         return sample
 
     def collate_dia_samples(self, batch: List[DiaSample]) -> DiaBatch:
-        texts_b = [b['text'] for b in batch]
-        labels_b = [b['labels'] for b in batch]
-        input_tok_b = [text.split(' ') for text in texts_b]
-        labels_tok_b = [label.split(' ') for label in labels_b]
-        print(f"input_tok {input_tok_b}")
-        print(F"labels_tok {labels_tok_b}")
+        # b['text'] and b['labels'] are two lists
+        input_tok_b = [b['text'] for b in batch]
+        labels_tok_b = [b['labels'] for b in batch]
+        # print(f"input_tok {input_tok_b}")
+        # print(F"labels_tok {labels_tok_b}")
         input_ids = [self.tokenizer.convert_tokens_to_ids(input_tok)
                      for input_tok in input_tok_b]
-        #labels_ids = [self.tokenizer.convert_tokens_to_ids(labels_tok)
-        #              for labels_tok in labels_tok_b]
+
         labels_ids = []
         for labels_tok in labels_tok_b:
             label_ids = []
             tok_length = len(labels_tok)
             for i in range(tok_length):
+                # first and last token is CLS and SEP, set as -100
                 if i == 0:
                     label_ids.append(-100)
                 elif i == tok_length - 1:
                     label_ids.append(-100)
                 else:
                     try:
-                        label_ids.append(labels[labels_tok[i]])
-                    except:
+                        label_ids.append(labels_dict[labels_tok[i]])
+                    except KeyError:
                         print("Label not found")
-                        label_ids.append(138)
+                        label_ids.append(len(labels_dict))
             labels_ids.append(label_ids)
-            print("encoded labels ids is: ", labels_ids)
-        print(labels_tok_b)
 
         batch = BatchEncoding({"input_ids": input_ids,
                                "labels": labels_ids})
@@ -93,11 +102,11 @@ class JointSluDataset(Dataset):
                     tokenizer: Tokenizer):
         path = None
         if split == 'train':
-            path = '../data/jointslu/bert_train.json'
+            path = '../data/jointslu/pre-train/b_train.json'
         elif split == 'test':
-            path = '../data/jointslu/bert_test.json'
+            path = '../data/jointslu/pre-train/b_test.json'
         elif split == 'val':
-            path = '../data/jointslu/bert_val.json'
+            path = '../data/jointslu/pre-train/b_val.json'
         if path:
             with open(path) as f:
                 annotations = json.load(f)
