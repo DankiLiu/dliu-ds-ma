e557c90d764421b5db2753f1364f0b734851f063
danqliu <danqing.liu@mailbox.org>
2022-11-22 16:56:26 +0100

    add comments

--- util.py
+++ util.py
@@ -7,6 +7,7 @@ import json
 
 def corenlp_server_start(path_to_jar=None,
                          path_to_models_jar=None):
+    """start corenlp server"""
     # Only in testing phase
     path_to_jar = 'C:/Users/liuda/Documents/Files/StudiumInDtl/Masterarbeit/corenlp/stanford-corenlp-4.5.0/stanford-corenlp-4.5.0.jar'
     path_to_models_jar = 'C:/Users/liuda/Documents/Files/StudiumInDtl/Masterarbeit/corenlp/stanford-corenlp-4.5.0/stanford-corenlp-4.5.0-models.jar'
@@ -23,11 +24,13 @@ def corenlp_server_start(path_to_jar=None,
 
 
 def corenlp_server_stop(server):
+    """stop corenlp server"""
     server.stop()
     print("Corenlp server stopped...")
 
 
 def server_is_running(url):
+    """return True if corenlp server is running, otherwise False"""
     try:
         page = requests.get(url)
         status_code = page.status_code
@@ -44,6 +47,7 @@ def server_is_running(url):
 
 
 def read_jointslu_labels():
+    """return a list of BIO labels for jointslu dataset"""
     with open("../data/jointslu.json") as f:
         data = json.load(f)
         jointslu_labels = data["jointslu_labels"]
@@ -52,8 +56,7 @@ def read_jointslu_labels():
 
 def save_jointslu_labels(new_labels):
     """Save all labels appeared in dataset into file
-    :paras new_labels: a list of label lists
-    """
+    :paras new_labels: a list of label lists"""
     data = {"jointslu_labels": list(new_labels)}
     print("label set", data)
     with open("../data/jointslu.json", 'w') as f:
@@ -75,6 +78,7 @@ def append_to_json(file_path, data):
 
 
 def read_from_json(path):
+    """read and return the json file content"""
     file = open(path, 'r')
     data = json.load(file)
     file.close()
@@ -82,11 +86,13 @@ def read_from_json(path):
 
 
 def read_keys_from_json(path, *args):
+    """return content of keys (args) of json file"""
     data = read_from_json(path)
     values = {}
     for arg in args:
         try:
             values[arg] = [item[arg] for item in data]
+            print("add item to values: ", values)
         except KeyError:
             print(f"Missing key {arg} in {path} file")
     return values
