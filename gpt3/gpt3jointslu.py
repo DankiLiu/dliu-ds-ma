3b1671452f31ebff26f52e1057d9decd2c0c1621
danqliu <danqing.liu@mailbox.org>
2022-12-02 13:51:04 +0100

    test gpt3, fit code to gpt3 access limit

--- gpt3/gpt3jointslu.py
+++ gpt3/gpt3jointslu.py
@@ -6,6 +6,7 @@ from gpt3.gpt3_util import get_example_by_sim, load_examples, \
     construct_oneshot_example, get_examples_gpt3, get_oneshot_prompt
 import openai
 import os
+import time
 
 from util import append_to_json
 
@@ -42,32 +43,50 @@ def zero_shot(prompt, sentences: List):
 def one_shot(prompt, sentences: List, oneshot_examples: List):
     # should be one example for each sentence
     assert len(sentences) == len(oneshot_examples)
+    print(f"one-shot predicting for {len(sentences)} tests")
     results = []
+    # gpt3 access limit 60 times per minute
     for i in range(len(sentences)):
+        time.sleep(2.0)
         prompt_i = get_oneshot_prompt(prompt, oneshot_examples[i], sentences[i])
+        try:
+            response = openai.Completion.create(engine="text-davinci-002",
+                                                prompt=prompt_i,
+                                                temperature=0,
+                                                max_tokens=256,
+                                                top_p=1.0)
+            results.append(response["choices"][0]["text"])
+        except openai.error.RateLimitError:
+            print("openai.error.RateLimitError")
+    return results
 
+
+def one_shot_per_example(prompt, sentence, oneshot_example):
+    prompt_i = get_oneshot_prompt(prompt, oneshot_example, sentence)
+    try:
         response = openai.Completion.create(engine="text-davinci-002",
                                             prompt=prompt_i,
                                             temperature=0,
                                             max_tokens=256,
                                             top_p=1.0)
-        results.append(response["choices"][0]["text"])
-        print("result:\n", response["choices"][0]["text"])
-    return results
+        print(f"response for \n{sentence}\n", response["choices"][0]["text"])
+        return response["choices"][0]["text"]
+    except openai.error.RateLimitError:
+        print("rate limit error")
 
 
 def gpt3jointslu(num, select=True, file_path="data/jointslu/gpt3/gpt3_output.json"):
     """num: number of input texts to be tested,
     select==Ture: choose examples that is similar to given input text"""
-    results = []
     # load test examples
     global oneshot_examples
-    print(f"--- testing {num} examples ---")
     texts, labels = get_examples_gpt3("test", num=num)
+    num_examples = len(texts)
+    print(f"--- testing {len(texts)} examples ---")
     # construct texts and ground truths
     std_gts = []
     ts = [' '.join(t) for t in texts]
-    for i in range(num):
+    for i in range(num_examples):
         std_gt = get_std_gt(ts[i], labels[i])
         std_gts.append(std_gt)
     # load examples if choose==True
@@ -83,11 +102,10 @@ def gpt3jointslu(num, select=True, file_path="data/jointslu/gpt3/gpt3_output.jso
         # todo: here should pick from selected examples or from all training data?
         idxs = [randint(0, len(examples) - 1) for _ in range(len(ts))]
         oneshot_examples = construct_oneshot_example([examples[i] for i in idxs])
-    responses = one_shot(PROMPT_2, ts, oneshot_examples)
-
-    from datetime import date
-    timestamp = str(date.today())
-    for i in range(num):
+    for i in range(num_examples):
+        response = one_shot_per_example(PROMPT_2, ts[i], oneshot_examples[i])
+        from datetime import date
+        timestamp = str(date.today())
         exp_text, exp_gt = std_gpt3_example(examples[i])
         result = {
             "timestamp": timestamp,
@@ -96,9 +114,9 @@ def gpt3jointslu(num, select=True, file_path="data/jointslu/gpt3/gpt3_output.jso
             "exp_gt": exp_gt,
             "text": ts[i],
             "labels": labels[i],
-            "prediction": responses[i],
+            "prediction": response,
             "std_gt": std_gts[i]
         }
-        print(f"%%%{i}th {result}")
-        results.append(result)
-    append_to_json(file_path=file_path, data=results)
+        append_to_json(file_path=file_path, data=[result])
+        print(f"result is appended \n{result}")
+        time.sleep(2.0)
